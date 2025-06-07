b0e1eec8efa3d4cbeffe8e60d122a3c2502c7b84
idkidn <sola72485@gmail.com>
2025-06-07 14:14:58 +0300

    Add files via upload

---
+++ yandere-simulator/script.js
@@ -0,0 +1,17 @@
+
+ScrollReveal().reveal('.img-bottom', { reset: true, origin: 'bottom', delay: 700, distance: '550px' });
+ScrollReveal().reveal('.top-h2', { reset: true, origin: 'top', delay: 700, distance: '50px' });
+ScrollReveal().reveal('.p-left', { reset: true, origin: 'left', delay: 700, distance: '150px' });
+ScrollReveal().reveal('.p-right', { reset: true, origin: 'right', delay: 700, distance: '150px' });
+ScrollReveal().reveal('.Windows', { reset: true, origin: 'left', delay: 700, distance: '150px' });
+ScrollReveal().reveal('.poni', { reset: true, origin: 'right', delay: 700, distance: '150px' });
+ScrollReveal().reveal('.Edinorog', { reset: true, origin: 'left', delay: 700, distance: '150px' });
+ScrollReveal().reveal('.Unicorn', { reset: true, origin: 'right', delay: 700, distance: '150px' });
+ScrollReveal().reveal('.deee', { reset: true, origin: 'left', delay: 700, distance: '150px' });
+ScrollReveal().reveal('.trtrtr', { reset: true, origin: 'right', delay: 700, distance: '150px' });
+
+
+//   ScrollReveal().reveal('h2',{reset: true, origin: 'top',  delay: 300,  distance: '100px' });
+//   ScrollReveal().reveal('p',{ reset: true, delay: 700 , origin: 'bottom',  distance: '150px'});
+//   ScrollReveal().reveal('.nav-item', {reset: true, duration: 500, interval: 500 , origin: 'top',  distance: '50px'});
+
